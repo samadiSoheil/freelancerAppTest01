325f460ba604e6992b17b46b7b48a535d8ade0bd
itsSoheil <soheil.samadi79@gmail.com>
2024-06-18 14:17:20 +0330

    Create route /Auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Auth from "./pages/Auth.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
