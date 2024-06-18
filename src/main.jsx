db37b86a0e4d6aac92432655605defb495b09960
itsSoheil <soheil.samadi79@gmail.com>
2024-06-18 14:13:04 +0330

    Add / path in router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
