7cfc3d162acd82535ea2ef1e47a345f471ff5e30
itsSoheil <soheil.samadi79@gmail.com>
2024-06-18 15:02:49 +0330

    create SendOTPForm form jsx

--- src/features/auth/SendOTPForm.jsx
+++ src/features/auth/SendOTPForm.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 const SendOTPForm = () => {
+  const [phoneNumber, setPhoneNumber] = useState(null);
   return (
     <>
-      <h1>this is SendOTPForm component</h1>
+      <div className="w-full">
+        <form className="space-y-5">
+          <div>
+            <label htmlFor="phoneNumber">شماره موبایل</label>
+            <input
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              id="phoneNumber"
+              autoFocus="true"
+              type="text"
+              className="textFiled__input"
+            />
+          </div>
+          <button className="w-full btn btn--secondary">ارسال کد تایید</button>
+        </form>
+      </div>
     </>
   );
 };
