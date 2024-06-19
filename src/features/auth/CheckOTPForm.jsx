a835c00cb2ffbff8335d37550eee2d45adcb8df2
itsSoheil <soheil.samadi79@gmail.com>
2024-06-19 12:47:26 +0330

    Create CheckOTPForm and add to Auth

---
+++ src/features/auth/CheckOTPForm.jsx
@@ -0,0 +1,9 @@
+const CheckOTPForm = () => {
+  return (
+    <>
+      <h1>this is check</h1>
+    </>
+  );
+};
+
+export default CheckOTPForm;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,12 @@
+import CheckOTPForm from "../features/auth/CheckOTPForm";
 import SendOTPForm from "../features/auth/SendOTPForm";
 
 const Auth = () => {
   return (
     <>
       <div className="container w-full xl:max-w-xl h-screen flex flex-col justify-center items-center ba">
-        <SendOTPForm />
+        {/* <SendOTPForm /> */}
+        <CheckOTPForm />
       </div>
     </>
   );
