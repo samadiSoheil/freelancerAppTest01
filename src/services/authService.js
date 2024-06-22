71c0cc0e66484898f8f6755584edd043d296499b
itsSoheil <soheil.samadi79@gmail.com>
2024-06-22 15:13:47 +0330

    fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import Http from "./httpService";
 
 export const getOtp = (userNumber) => {
-  return Http.post("/user/get-otpq", userNumber).then(({ data }) => data.data);
+  return Http.post("/user/get-otp", userNumber).then(({ data }) => data.data);
 };
 
 export const checkOtp = (data) => {
