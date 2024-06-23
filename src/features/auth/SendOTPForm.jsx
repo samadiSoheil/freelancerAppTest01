5d4b344cd37f367da9facf415d18e59fffb55a52
itsSoheil <soheil.samadi79@gmail.com>
2024-06-23 12:15:04 +0330

    Create SendOTPForm component

--- src/features/auth/SendOTPForm.jsx
+++ src/features/auth/SendOTPForm.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import TextField from "../../ui/TextFIeld";
 import Button from "../../ui/Button";
+import Loading from "../../ui/Loading";
 
-const SendOTPForm = () => {
-  const [phoneNumber, setPhoneNumber] = useState(null);
+const SendOTPForm = ({ setPhoneNumber, phoneNumber, isPending, getOtpHandler }) => {
   return (
     <>
       <div className="w-full">
-        <form className="space-y-5">
+        <form onSubmit={getOtpHandler} className="space-y-5">
           <TextField
             id="phoneNumber"
             lableText="شماره موبایل"
             elemValue={phoneNumber}
             setElemVaulue={setPhoneNumber}
+            isFoucus={true}
           />
-          <Button>ارسال کد تایید</Button>
+          {isPending ? <Loading /> : <Button>ارسال کد تایید</Button>}
         </form>
       </div>
     </>
