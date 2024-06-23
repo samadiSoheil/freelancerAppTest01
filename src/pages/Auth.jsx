1fd9db87916460be275dad804ab4941843abe7a0
itsSoheil <soheil.samadi79@gmail.com>
2024-06-23 12:17:31 +0330

    Add AuthContainer to Auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,10 @@
-import CheckOTPForm from "../features/auth/CheckOTPForm";
-import SendOTPForm from "../features/auth/SendOTPForm";
+import AuthContainer from "../features/auth/AuthContainer";
 
 const Auth = () => {
   return (
     <>
       <div className="container w-full xl:max-w-xl h-screen flex flex-col justify-center items-center ba">
-        {/* <SendOTPForm /> */}
-        <CheckOTPForm />
+        <AuthContainer />
       </div>
     </>
   );
