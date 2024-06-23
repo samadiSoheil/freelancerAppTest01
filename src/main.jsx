e5f168a90cde4bb9509ff61c2d3d9902b16ef22b
itsSoheil <soheil.samadi79@gmail.com>
2024-06-23 12:27:13 +0330

    Add new path for CompleteProfile page

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Auth from "./pages/Auth.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import CompleteProfile from "./pages/CompleteProfile.jsx";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         <Auth />
       </QueryClientProvider>
     ),
+  },
+  {
+    path: "/complete-profile",
+    element: <CompleteProfile />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
