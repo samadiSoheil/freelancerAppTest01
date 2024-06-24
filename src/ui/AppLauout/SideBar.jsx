a3b4c55cc2b7a2bffac1754625cdc688a63cbb22
itsSoheil <soheil.samadi79@gmail.com>
2024-06-24 17:06:09 +0330

    Create SideBar component for AppLayout

---
+++ src/ui/AppLauout/SideBar.jsx
@@ -0,0 +1,13 @@
+const SideBar = () => {
+  return (
+    <>
+      <div className="grid justify-stretch items-stretch col-span-1 row-span-2 p-2 bg-slate-400">
+        <div className="  bg-red-300">
+          1<h1>this is SideBar</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
