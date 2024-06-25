74f9ae7c653f78d3fdb8d16fe6b1e9ce96edfd64
itsSoheil <soheil.samadi79@gmail.com>
2024-06-25 17:57:22 +0330

    Change route

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
@@ -10,6 +10,7 @@ import CompleteProfile from "./pages/CompleteProfile.jsx";
 import AppLayout from "./ui/AppLauout/AppLayout.jsx";
 import Owner from "./pages/Owner.jsx";
 import Projects from "./pages/Projects.jsx";
+import Project from "./pages/Project.jsx";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
@@ -31,12 +32,20 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Navigate to="dashboard" replace={true} />,
+      },
+      {
+        path: "dashboard",
         element: <Owner />,
       },
       ,
       {
         path: "projects",
         element: <Projects />,
+      },
+      {
+        path: "w",
+        element: <Project />,
       },
     ],
   },
