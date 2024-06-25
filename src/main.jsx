b80d19ae0f51af10c8dcff0283ffc839ae896e58
itsSoheil <soheil.samadi79@gmail.com>
2024-06-25 13:38:48 +0330

    Add Owner page to router

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import Auth from "./pages/Auth.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile.jsx";
+import AppLayout from "./ui/AppLauout/AppLayout.jsx";
+import Owner from "./pages/Owner.jsx";
+import Projects from "./pages/Projects.jsx";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
@@ -21,6 +24,16 @@ const router = createBrowserRouter([
   {
     path: "/complete-profile",
     element: <CompleteProfile />,
+  },
+  {
+    path: "/owner",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Owner />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
