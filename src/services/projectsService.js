8a198aa97202362456cf27d3a7646a787a43d7d1
itsSoheil <soheil.samadi79@gmail.com>
2024-06-26 17:38:55 +0330

    Create deleteOwnerProject service for deleting project

--- src/services/projectsService.js
+++ src/services/projectsService.js
@@ -3,3 +3,7 @@ import Http from "./httpService";
 export const getAllOwnerProjects = () => {
   return Http.get("/project/owner-projects").then(({ data }) => data.data);
 };
+
+export const deleteOwnerProject = (id) => {
+  return Http.delete(`/project/${id}`).then(({ data }) => data.data);
+};
