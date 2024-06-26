8fb999f8a2d96a7f6e2639c85baf51e3b5867db6
itsSoheil <soheil.samadi79@gmail.com>
2024-06-26 22:47:34 +0330

    create outside click func for closing modal

--- src/ui/modal/Modal.jsx
+++ src/ui/modal/Modal.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useRef } from "react";
+
 const Modal = ({ isOpen, onClose, title, typeOfModal, children }) => {
+  const refElem = useRef();
+
+  useEffect(() => {
+    const clickHandler = (e) => {
+      if (
+        refElem.current &&
+        !refElem.current.contains(e.target) &&
+        e.target.id == "modal-container"
+      ) {
+        onClose(false);
+      }
+    };
+    document.addEventListener("click", clickHandler);
+
+    return () => document.removeEventListener("click", clickHandler);
+  }, []);
+
   return (
     <>
       {isOpen && (
-        <div className="w-full h-screen fixed backdrop-blur-sm bg-slate-900/50  left-0 top-0 z-50 ">
-          {" "}
-          <div className="w-[calc(100vw-3rem)] sm:w-[600px] max-h-[calc(100vh-3rem)] overflow-y-auto fixed top-1/2 left-1/2 p-5 rounded-lg backdrop-blur-0 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 shadow-xl transition-all duration-500">
+        <div
+          id="modal-container"
+          className="w-full h-screen fixed backdrop-blur-sm bg-slate-900/50  left-0 top-0 z-50 "
+        >
+          <div
+            ref={refElem}
+            className="w-[calc(100vw-3rem)] sm:w-[600px] max-h-[calc(100vh-3rem)] overflow-y-auto fixed top-1/2 left-1/2 p-5 rounded-lg backdrop-blur-0 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 shadow-xl transition-all duration-500"
+          >
             <div className="flex justify-between items-center border-b pb-5">
               <h1
                 className={`border-r-8 font-Vazir-Bold  px-2 ${
