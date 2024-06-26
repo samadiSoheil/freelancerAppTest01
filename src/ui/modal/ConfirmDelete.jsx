f572c04de77ca453e5da29872d51421c08999d58
itsSoheil <soheil.samadi79@gmail.com>
2024-06-26 16:43:32 +0330

    Create ConfirmDelete for delete Modal

---
+++ src/ui/modal/ConfirmDelete.jsx
@@ -0,0 +1,22 @@
+const ConfirmDelete = ({ title, onClose }) => {
+  return (
+    <div className="pt-4">
+      <div className="flex flex-col items-start gap-3 text-secondary-800 text-right">
+        <h1 className="font-Vazir-Bold ">{title}</h1>
+        <p>درصورت حذف پروژه قابل بازیابی مجدد نمی باشد.</p>
+      </div>
+      <div className="flex justify-between items-center gap-5 mt-5">
+        <button
+          className="btn btn--primery w-full font-Vazir-Bold"
+          onClick={() => onClose(false)}
+        >
+          خیر
+        </button>
+        <button className="btn btn--secondary__outline w-full font-Vazir-Bold">
+          بله
+        </button>
+      </div>
+    </div>
+  );
+};
+export default ConfirmDelete;
