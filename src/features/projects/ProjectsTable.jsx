548344b5f78dad78066517645c3caf19f51a559d
itsSoheil <soheil.samadi79@gmail.com>
2024-06-26 16:41:49 +0330

    Add Table in ProjectsTable component

--- src/features/projects/ProjectsTable.jsx
+++ src/features/projects/ProjectsTable.jsx
@@ -1,4 +1,6 @@
 import Loading from "../../ui/Loading";
+import Table from "../../ui/table/Table";
+import ProjectRow from "./ProjectRow";
 import { useOwnerProject } from "./useOwnerProject";
 
 const ProjectsTable = () => {
@@ -9,7 +11,28 @@ const ProjectsTable = () => {
   if (!projects?.length) return <p>هیچی نیست...</p>;
   return (
     <>
-      <h1>this is Projects</h1>
+      <Table>
+        <Table.Hearder>
+          <th>#</th>
+          <th>عنوان پروژه</th>
+          <th>دسته بندی</th>
+          <th>بودجه</th>
+          <th>ددلاین</th>
+          <th>تگ ها</th>
+          <th>فریلنسر</th>
+          <th>وضعیت</th>
+          <th>عملیات</th>
+        </Table.Hearder>
+        <Table.Body>
+          {projects?.map((item, index) => {
+            return (
+              <Table.Row key={item._id}>
+                <ProjectRow item={item} index={index} />
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
     </>
   );
 };
