1796a0479a1c23e36b5541f1343843dbb907be19
itsSoheil <soheil.samadi79@gmail.com>
2024-06-30 16:32:10 +0330

    Create DatePickerField component

---
+++ src/ui/DatePickerField.jsx
@@ -0,0 +1,23 @@
+import DatePicker from "react-multi-date-picker";
+import persian_fa from "react-date-object/locales/persian_fa";
+import persian from "react-date-object/calendars/persian";
+
+const DatePickerField = ({ label, date, setDate }) => {
+  console.log(date);
+  return (
+    <div className="flex flex-col space-y-3">
+      <label>{label}</label>
+      <DatePicker
+        containerClassName="w-full"
+        inputClass="textFiled__input"
+        value={date}
+        onChange={(date) => setDate(date)}
+        calendar={persian}
+        locale={persian_fa}
+        calendarPosition="bottom-center"
+      />
+    </div>
+  );
+};
+
+export default DatePickerField;
