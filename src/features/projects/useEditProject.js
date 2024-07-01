fab5f2bd358b9e92ab497588a1383c71dbe82cdc
itsSoheil <soheil.samadi79@gmail.com>
2024-07-01 14:06:48 +0330

    Create useEditProject custom hook for editing project

---
+++ src/features/projects/useEditProject.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { editProject } from "../../services/projectsService";
+import toast from "react-hot-toast";
+
+const useEditProject = () => {
+  const queryClient = useQueryClient();
+
+  const { isPending: isEditing, mutate: editeProjectFun } = useMutation({
+    mutationFn: editProject,
+    onSuccess: (data) => {
+      toast.success(data?.message);
+      queryClient.invalidateQueries({
+        queryKey: ["owner-projects"],
+      });
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
+  return { isEditing, editeProjectFun };
+};
+
+export default useEditProject;
