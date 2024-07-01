6149b53f0ab6121267f5aeae2e13047382f72ad3
itsSoheil <soheil.samadi79@gmail.com>
2024-07-01 11:28:32 +0330

    Create usePostProject custom hook

---
+++ src/features/projects/usePostProject.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createProject } from "../../services/projectsService";
+import toast from "react-hot-toast";
+
+const usePostProject = () => {
+  const queryClient = useQueryClient();
+  const { isPending: isCreatingProjrct, mutate: createProjectFn } = useMutation({
+    mutationFn: createProject,
+    onSuccess: (data) => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({
+        queryKey: ["owner-projects"],
+      });
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
+  return { isCreatingProjrct, createProjectFn };
+};
+
+export default usePostProject;
