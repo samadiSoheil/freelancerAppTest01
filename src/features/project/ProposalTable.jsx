b74ba0d1fad139a390eae7bf10cf738c25a31a6b
itsSoheil <soheil.samadi79@gmail.com>
2024-07-02 16:25:05 +0330

    Create ProposalTable

---
+++ src/features/project/ProposalTable.jsx
@@ -0,0 +1,33 @@
+import Table from "../../ui/table/Table";
+import ProposalsRow from "./ProposalsRow";
+
+const ProposalTable = ({ project }) => {
+  const { proposals } = project;
+  if (!proposals.length) return <p>nothing here...</p>;
+  return (
+    <>
+      <Table>
+        <Table.Hearder>
+          <th>#</th>
+          <th>فریلنسر</th>
+          <th>توضیحات</th>
+          <th>تحویل</th>
+          <th>هزینه</th>
+          <th>وضعیت</th>
+          <th>عملیات</th>
+        </Table.Hearder>
+        <Table.Body>
+          {proposals?.map((item, index) => {
+            return (
+              <Table.Row key={item._id}>
+                <ProposalsRow item={item} index={index} projectTitle={project.title} />
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </>
+  );
+};
+
+export default ProposalTable;
