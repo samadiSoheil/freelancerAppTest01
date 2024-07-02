a2ab2a62fa5550ad20ddd013035522ab6dc2e905
itsSoheil <soheil.samadi79@gmail.com>
2024-07-02 16:39:16 +0330

    Add ProjectHeader ProposalTable component and useProject custom hook

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,8 +1,17 @@
+import ProjectHeader from "../features/project/ProjectHeader";
+import ProposalTable from "../features/project/ProposalTable";
+import useProject from "../features/project/useProject";
+import Loading from "../ui/Loading";
+
 const Project = () => {
+  const { project, isPending } = useProject();
+  if (isPending) return <Loading />;
+  if (!project) return <p>nothing here...</p>;
   return (
-    <>
-      <h1>this is project</h1>
-    </>
+    <div className="w-full">
+      <ProjectHeader project={project} />
+      <ProposalTable project={project} />
+    </div>
   );
 };
 
