85c12fb709b95636d48c074134a4b1cd79abd63d
itsSoheil <soheil.samadi79@gmail.com>
2024-07-02 16:29:32 +0330

    Create useProposalStatus custom hook for getting project proposals

---
+++ src/features/project/useProposalStatus.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { setProposalStatus } from "../../services/proposalService";
+
+const useProposalStatus = () => {
+  const { mutate: changingProposalStatusFun, isPending: isChangeStatus } = useMutation({
+    mutationFn: setProposalStatus,
+    onSuccess: (data) => {
+      toast.success(data?.message);
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
+  return { isChangeStatus, changingProposalStatusFun };
+};
+
+export default useProposalStatus;
