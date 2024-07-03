4a4f7960f0df8b7719d0cdfb666b56fbd5bb8510
itsSoheil <soheil.samadi79@gmail.com>
2024-07-03 10:25:17 +0330

    Create DarkModeToggle

---
+++ src/ui/AppLauout/HeaderMenu/DarkModeToggle.jsx
@@ -0,0 +1,12 @@
+import { HiOutlineSun } from "react-icons/hi2";
+import { HiOutlineMoon } from "react-icons/hi2";
+
+const DarkModeToggle = () => {
+  return (
+    <button>
+      <HiOutlineSun className="size-6" />
+    </button>
+  );
+};
+
+export default DarkModeToggle;
