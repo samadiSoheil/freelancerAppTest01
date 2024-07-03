eb6bb4cfc7a249439b372cdb652c51bb46a482a6
itsSoheil <soheil.samadi79@gmail.com>
2024-07-03 10:23:33 +0330

    Create UserAvatar component

---
+++ src/features/auth/UserAvatar.jsx
@@ -0,0 +1,17 @@
+import { useUser } from "./useUser";
+
+const UserAvatar = () => {
+  const {
+    data: { user },
+  } = useUser();
+  console.log(user);
+
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <img className="size-10 rounded-full" src="/user.jpg" alt={user?.name} />
+      <span className="font-Vazir-Medium">{user?.name}</span>
+    </div>
+  );
+};
+
+export default UserAvatar;
