1d5e944df8be11ed0a88ef61cb1129538b8e0227
itsSoheil <soheil.samadi79@gmail.com>
2024-07-03 10:56:55 +0330

    Add logout logic

--- src/ui/AppLauout/HeaderMenu/Logout.jsx
+++ src/ui/AppLauout/HeaderMenu/Logout.jsx
@@ -1,8 +1,16 @@
 import { HiOutlineArrowLeftStartOnRectangle } from "react-icons/hi2";
+import useLogoutUser from "../../../features/auth/useLogoutUser";
+import Loading from "../../Loading";
 
 const Logout = () => {
-  return (
-    <button>
+  const { isLogouting, logoutFun } = useLogoutUser();
+  const logoutHandler = () => {
+    logoutFun();
+  };
+  return isLogouting ? (
+    <Loading />
+  ) : (
+    <button onClick={logoutHandler}>
       <HiOutlineArrowLeftStartOnRectangle className="size-6 text-red-500" />
     </button>
   );
