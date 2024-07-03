31a6fad98783b1a88e557d884240048404adaae2
itsSoheil <soheil.samadi79@gmail.com>
2024-07-03 14:39:47 +0330

    Create change theme logic

--- src/ui/AppLauout/HeaderMenu/DarkModeToggle.jsx
+++ src/ui/AppLauout/HeaderMenu/DarkModeToggle.jsx
@@ -1,10 +1,19 @@
 import { HiOutlineSun } from "react-icons/hi2";
 import { HiOutlineMoon } from "react-icons/hi2";
+import { useTheme } from "../../../context/ThemeProvider";
 
 const DarkModeToggle = () => {
+  const { isDark, changeThemeFun } = useTheme();
+  const changeThemeHandler = () => {
+    changeThemeFun();
+  };
   return (
-    <button>
-      <HiOutlineSun className="size-6" />
+    <button onClick={changeThemeHandler}>
+      {isDark ? (
+        <HiOutlineMoon className="size-6" />
+      ) : (
+        <HiOutlineSun className="size-6" />
+      )}
     </button>
   );
 };
