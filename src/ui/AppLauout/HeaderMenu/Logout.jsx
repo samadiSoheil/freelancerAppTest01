8ca7c0df6df40160a45892a759e74b8848fe0b16
itsSoheil <soheil.samadi79@gmail.com>
2024-07-03 10:24:34 +0330

    Create logout component

---
+++ src/ui/AppLauout/HeaderMenu/Logout.jsx
@@ -0,0 +1,11 @@
+import { HiOutlineArrowLeftStartOnRectangle } from "react-icons/hi2";
+
+const Logout = () => {
+  return (
+    <button>
+      <HiOutlineArrowLeftStartOnRectangle className="size-6 text-red-500" />
+    </button>
+  );
+};
+
+export default Logout;
