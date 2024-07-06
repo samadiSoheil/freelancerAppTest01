98dbd0ef08246258807a296abf8b1a72897ef63b
itsSoheil <soheil.samadi79@gmail.com>
2024-07-06 11:21:27 +0330

    Change code with HeaderOfComponent

--- src/features/project/ProjectHeader.jsx
+++ src/features/project/ProjectHeader.jsx
@@ -1,22 +1,12 @@
-import { FaLongArrowAltRight } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import HeaderOfComponent from "../../ui/AppLauout/HeaderOfComponent";
+import BackPageBtn from "../../ui/AppLauout/BackPageBtn";
 
 const ProjectHeader = ({ project }) => {
-  const navigateUser = useNavigate();
   return (
     <>
-      <div className="flex justify-between font-Vazir-Bold text-secondary-800 mb-8">
-        <h1 className="font-Vazir-Bold text-2xl  text-secondary-800 dark:text-secondary-100 border-r-8 border-r-primary-800 pr-2 leading-[48px]">
-          لسیت درخواست های پروژه ی<span>&nbsp;{project.title}</span>
-        </h1>
-        {/* BACK PAGE BTN */}
-        <button
-          className="border border-gray-500 size-10 flex justify-center items-center rounded-xl"
-          onClick={() => navigateUser(-1)}
-        >
-          <FaLongArrowAltRight className="size-6 text-gray-500 rotate-180" />
-        </button>
-      </div>
+      <HeaderOfComponent title={` لسیت درخواست های پروژه ی ${project.title}`}>
+        <BackPageBtn />
+      </HeaderOfComponent>
     </>
   );
 };
