37b19b2d1d923c306afab51d176adce1e607f396
itsSoheil <soheil.samadi79@gmail.com>
2024-07-06 11:20:41 +0330

    Change code with HeaderOfComponent

--- src/features/projects/ProjectsHeader.jsx
+++ src/features/projects/ProjectsHeader.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import Modal from "../../ui/modal/Modal";
 import AddProjectForm from "./AddProjectForm";
+import HeaderOfComponent from "../../ui/AppLauout/HeaderOfComponent";
 
 const ProjectsHeader = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <>
-      <div className="flex justify-between items-center my-8">
-        <h1 className="font-Vazir-Bold text-2xl text-secondary-800 dark:text-secondary-100 border-r-8  border-r-primary-800 pr-2 leading-[48px]">
-          پروژه های شما
-        </h1>
+      <HeaderOfComponent title=" پروژه های شما">
         <button
           onClick={() => setIsOpenModal(true)}
           className="btn btn--primery flex gap-2"
@@ -25,7 +23,7 @@ const ProjectsHeader = () => {
         >
           <AddProjectForm setIsOpenModal={setIsOpenModal} />
         </Modal>
-      </div>
+      </HeaderOfComponent>
     </>
   );
 };
