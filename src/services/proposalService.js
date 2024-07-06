28c7348b4819eef4419a04370f11307ee97f5132
itsSoheil <soheil.samadi79@gmail.com>
2024-07-06 15:51:38 +0330

    Create getAllProposals service for getting all of sended proposals

--- src/services/proposalService.js
+++ src/services/proposalService.js
@@ -3,3 +3,7 @@ import Http from "../services/httpService";
 export const setProposalStatus = ({ id, data }) => {
   return Http.patch(`/proposal/${id}`, data).then(({ data }) => data.data);
 };
+
+export const getAllProposals = () => {
+  return Http.get("/proposal/list").then(({ data }) => data.data);
+};
