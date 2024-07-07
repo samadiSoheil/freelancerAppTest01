4d724329b287f84ea459db8d4afd1e93507fb090
itsSoheil <soheil.samadi79@gmail.com>
2024-07-07 17:22:38 +0330

    Create SubmitedProjects page

---
+++ src/pages/SubmitedProjects.jsx
@@ -0,0 +1,11 @@
+import AllProjectsLayout from "../features/Freelancer/projects/AllProjectsLayout";
+
+const SubmitedProjects = () => {
+  return (
+    <>
+      <AllProjectsLayout />
+    </>
+  );
+};
+
+export default SubmitedProjects;
