32199442f6f9b8517839902f5fec7112b7134856
itsSoheil <soheil.samadi79@gmail.com>
2024-07-08 14:31:28 +0330

    Add Filter & FilterDropDown component for filter all projects

--- src/features/Freelancer/projects/AllProjectsLayout.jsx
+++ src/features/Freelancer/projects/AllProjectsLayout.jsx
@@ -1,12 +1,41 @@
+import useCategory from "../../../hook/useCategory";
 import BackPageBtn from "../../../ui/AppLauout/BackPageBtn";
 import HeaderOfComponent from "../../../ui/AppLauout/HeaderOfComponent";
+import Filter from "../../../ui/FilterComponents/Filter";
+import FilterDropDown from "../../../ui/FilterComponents/FilterDropDown";
 import ProjectsTable from "./ProjectsTable";
 
 const AllProjectsLayout = () => {
+  const { isLoading, transformedCategory } = useCategory();
   return (
     <>
       <HeaderOfComponent title="پروژه ها">
-        <BackPageBtn />
+        <div className="flex justify-end items-center gap-8">
+          <Filter
+            label="وضعیت"
+            filterField="status"
+            optioins={[
+              { value: "ALL", label: "همه" },
+              { value: "OPEN", label: "باز" },
+              { value: "CLOSED", label: "بسته" },
+            ]}
+          />
+          <FilterDropDown
+            filterField="category"
+            optioins={[{ value: "ALL", label: "همه" }, ...transformedCategory]}
+            isLoading={isLoading}
+          />
+          <FilterDropDown
+            filterField="sort"
+            optioins={[
+              { value: "ALL", label: "پیش فرض" },
+              { value: "latest", label: "جدیدترین" },
+              { value: "earliest", label: "قدیمی ترین" },
+            ]}
+            isLoading={isLoading}
+          />
+          <BackPageBtn />
+        </div>
       </HeaderOfComponent>
       <ProjectsTable />
     </>
