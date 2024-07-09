77c0b3828507c45665b645ef3f0190929f610047
itsSoheil <soheil.samadi79@gmail.com>
2024-07-09 14:17:41 +0330

    Create AdminLayout

---
+++ src/features/admin/AdminLayout.jsx
@@ -0,0 +1,35 @@
+import { MdHome } from "react-icons/md";
+import AppLayout from "../../ui/AppLauout/AppLayout";
+import CustomNavLink from "../../ui/AppLauout/CustomNavLink";
+import SideBar from "../../ui/AppLauout/SideBar";
+import { HiMiniArrowsPointingOut, HiSquare3Stack3D, HiUser } from "react-icons/hi2";
+
+const AdminLayout = () => {
+  return (
+    <AppLayout>
+      <SideBar>
+        <CustomNavLink
+          link="dashboard"
+          iconComponent={<MdHome className="size-5" />}
+          title="خانه"
+        />
+        <CustomNavLink
+          link="users"
+          iconComponent={<HiUser className="size-5" />}
+          title="کاربران"
+        />
+        <CustomNavLink
+          link="proposals"
+          iconComponent={<HiMiniArrowsPointingOut className="size-5" />}
+          title="درخواست ها"
+        />
+        <CustomNavLink
+          link="projects"
+          iconComponent={<HiSquare3Stack3D className="size-5" />}
+          title="پروژه ها"
+        />
+      </SideBar>
+    </AppLayout>
+  );
+};
+export default AdminLayout;
