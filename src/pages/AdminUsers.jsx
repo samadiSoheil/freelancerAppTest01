6727f7d82933c3ab1d106a484cc7ab924df1cd61
itsSoheil <soheil.samadi79@gmail.com>
2024-07-09 16:19:18 +0330

    Create AdminUsers page

---
+++ src/pages/AdminUsers.jsx
@@ -0,0 +1,11 @@
+import UseersLayout from "../features/admin/users/UseersLayout";
+
+const AdminUsers = () => {
+  return (
+    <>
+      <UseersLayout />
+    </>
+  );
+};
+
+export default AdminUsers;
