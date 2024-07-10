a71621858e0c5bc022bdf35112dbeefdc9179bcb
itsSoheil <soheil.samadi79@gmail.com>
2024-07-10 22:23:56 +0330

    Add close sidebar with change pathname of location

--- src/ui/protectedRoute/ProtectedRoute.jsx
+++ src/ui/protectedRoute/ProtectedRoute.jsx
@@ -1,10 +1,21 @@
 import { useEffect } from "react";
 import { useAuthorize } from "../../features/auth/useAuthorize";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useTheme } from "../../context/ThemeProvider";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isAuthorise, isVerirified, isPending } = useAuthorize();
+
+  //* this is for closing fixed menu
+  const { pathname } = useLocation();
+  const { setISopenSidebar } = useTheme();
+  const isChangePage = pathname.split("/").at(-1);
+  useEffect(() => {
+    setISopenSidebar(false);
+  }, [isChangePage]);
+  //* this is for closing fixed menu
+
   const navigateUser = useNavigate();
   useEffect(() => {
     if (!isPending && !isAuthenticated) navigateUser("/auth");
