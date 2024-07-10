0800e2257ef571e01d6e7b1c124f304900c9a168
itsSoheil <soheil.samadi79@gmail.com>
2024-07-10 22:26:25 +0330

    Change logic for admin and owner

--- src/features/projects/ProjectsTable.jsx
+++ src/features/projects/ProjectsTable.jsx
@@ -1,14 +1,15 @@
+import { useAllProjects } from "../../hook/useAllProjects";
 import Loading from "../../ui/Loading";
 import Table from "../../ui/table/Table";
 import ProjectRow from "./ProjectRow";
 import { useOwnerProject } from "./useOwnerProject";
 
-const ProjectsTable = () => {
-  const { isPending: isLoading, projects } = useOwnerProject();
-  console.log(projects);
-
-  if (isLoading) return <Loading />;
-  if (!projects?.length) return <p>هیچی نیست...</p>;
+const ProjectsTable = ({ isAdmin = false }) => {
+  const { isPending: isLoading, projects: projectsForOwner } = useOwnerProject();
+  const { projects, isPending } = useAllProjects();
+  if (isLoading || isPending) return <Loading />;
+  if (!projectsForOwner?.length || !projects.length) return <p>هیچی نیست...</p>;
+  let arrayToMap = isAdmin ? projects : projectsForOwner;
   return (
     <>
       <Table>
@@ -25,7 +26,7 @@ const ProjectsTable = () => {
           <th>درخواست ها</th>
         </Table.Hearder>
         <Table.Body>
-          {projects?.map((item, index) => {
+          {arrayToMap?.map((item, index) => {
             return (
               <Table.Row key={item._id}>
                 <ProjectRow item={item} index={index} />
